29a0a756cbdad282ae5d51268b68b62d193a5666
andersonkaiti <anderkaiti@gmail.com>
2025-09-23 09:38:58 -0300

    :wrench: chore(webpack): update build process with Webpack integration and adjust Babel output directory

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('node:path')
+
+/** @type {import('webpack').Configuration} */
+const config = {
+  entry: path.resolve(__dirname, 'transpiled', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+}
+
+module.exports = config
