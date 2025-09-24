0966e4610c8be086ae1026fbbd8e767fdefbb2f9
andersonkaiti <anderkaiti@gmail.com>
2025-09-24 08:09:53 -0300

    :sparkles: feat(App): create first App component and update index to render it

---
+++ src/App.jsx
@@ -0,0 +1,3 @@
+export default function App() {
+  return <h1>Componente App</h1>
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(<h1>Hello, World!</h1>)
+createRoot(document.getElementById('root')).render(<App />)
