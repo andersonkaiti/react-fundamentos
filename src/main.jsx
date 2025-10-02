3e7e98c7b6872aab200b3e50207e75a57e64c89b
andersonkaiti <anderkaiti@gmail.com>
2025-10-02 11:23:31 -0300

    :wrench: chore(styled-components): add styled-components for global styles and remove SCSS file

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import App from './components/App'
-import './index.scss'
+import GlobalStyle from './styles/global'
 
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById('root')).render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>
+)
