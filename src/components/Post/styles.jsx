18596efc072dac8968a684c1c766bdf112aa7f8b
andersonkaiti <anderkaiti@gmail.com>
2025-10-06 11:43:53 -0300

    :recycle: refactor(Post,styled-components): simplify dynamic opacity and color logic using css helper for clearer removed state handling and enabled syntax highlighting

--- src/components/Post/styles.jsx
+++ src/components/Post/styles.jsx
@@ -1,12 +1,17 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 const REMOVED_OPACITY = 0.5
 const FULL_OPACITY = 1
 
 export const Container = styled.article`
   margin-bottom: 24px;
-  opacity: ${({ removed }) => (removed ? REMOVED_OPACITY : FULL_OPACITY)};
-  color: ${({ removed }) => (removed ? '#f00' : '#fff')};
+  /* opacity: ${({ removed }) => (removed ? REMOVED_OPACITY : FULL_OPACITY)}; */
+  /* color: ${({ removed }) => (removed ? '#f00' : '#fff')}; */
+
+  ${({ removed }) => css`
+    opacity: ${removed ? REMOVED_OPACITY : FULL_OPACITY};
+    color: ${removed ? '#f00' : '#fff'};
+  `}
 `
 
 export const Subtitle = styled.small`
