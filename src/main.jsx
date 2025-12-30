cb5fb26663162adddf3b876790932b65487e63f6
andersonkaiti <anderkaiti@gmail.com>
2025-12-30 20:30:58 -0300

    :sparkles: feat(theme): implement light and dark themes with styled-components and update global styles for dynamic theming

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
 import Layout from './components/Layout'
 import GlobalStyle from './styles/global'
+import themes from './styles/themes'
 
 createRoot(document.getElementById('root')).render(
-  <>
+  <ThemeProvider theme={themes.light}>
     <GlobalStyle />
     <Layout />
-  </>
+  </ThemeProvider>
 )
