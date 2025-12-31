dfc4eea73d204553b1686d8a2fd7012a6cc149c4
andersonkaiti <anderkaiti@gmail.com>
2025-12-31 19:43:35 -0300

    :sparkles: feat(theme,localstorage): enhance ThemeProvider to persist theme selection in localStorage and initialize theme from localStorage

--- src/contexts/ThemeProvider.jsx
+++ src/contexts/ThemeProvider.jsx
@@ -4,13 +4,21 @@ import themes from '../styles/themes'
 
 export const ThemeContext = createContext()
 
+const THEME_KEY = 'theme'
+
 export default function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState('dark')
+  const defaultTheme = localStorage.getItem(THEME_KEY) || 'dark'
+
+  const [theme, setTheme] = useState(defaultTheme)
 
   const currentTheme = useMemo(() => themes[theme] || themes.dark, [theme])
 
   function handleToggleTheme() {
-    setTheme(theme === 'dark' ? 'light' : 'dark')
+    const nextTheme = theme === 'dark' ? 'light' : 'dark'
+
+    setTheme(nextTheme)
+
+    localStorage.setItem(THEME_KEY, nextTheme)
   }
 
   return (
