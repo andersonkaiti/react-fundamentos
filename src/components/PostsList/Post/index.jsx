cdee25815a4444297c1235cc5b7f55043fbf64ef
andersonkaiti <anderkaiti@gmail.com>
2026-01-01 19:58:24 -0300

    :sparkles: feat(class,props): refactor Post component to class-based syntax and demonstrate props usage

--- src/components/PostsList/Post/index.jsx
+++ src/components/PostsList/Post/index.jsx
@@ -1,10 +1,16 @@
+import { Component } from 'react'
 import { Container } from './styles'
 
-export default function Post({ title, description }) {
-  return (
-    <Container>
-      <h2>{title}</h2>
-      <small>{description}</small>
-    </Container>
-  )
+export default class Post extends Component {
+  render() {
+    // As props ficam dentro da instância da classe: this
+    const { title, description } = this.props
+
+    return (
+      <Container>
+        <h2>{title}</h2>
+        <small>{description}</small>
+      </Container>
+    )
+  }
 }
--- src/components/PostsList/index.jsx
+++ src/components/PostsList/index.jsx
@@ -3,9 +3,7 @@ import Post from './Post'
 import posts from './posts'
 import { Container } from './styles'
 
-// Todo componente de classe deve estender a classe Component
 export default class PostsList extends Component {
-  // Método obrigatório e responsável por renderizar o JSX
   render() {
     return (
       <Container>
