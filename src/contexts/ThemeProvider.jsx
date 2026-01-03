005b6fb6cf3c8ed2dc60f0b8abc6889847b81937
andersonkaiti <anderkaiti@gmail.com>
2026-01-03 00:32:00 -0300

    :recycle: refactor(ThemeProvider): convert ThemeProvider to a class-based component and manage theme state using class state and lifecycle methods

--- src/contexts/ThemeProvider.jsx
+++ src/contexts/ThemeProvider.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useMemo, useState } from 'react'
+import { Component, createContext, useContext } from 'react'
 import { ThemeProvider as StyledComponentsThemeProvider } from 'styled-components'
 import themes from '../styles/themes'
 
@@ -6,33 +6,33 @@ export const ThemeContext = createContext()
 
 const THEME_KEY = 'theme'
 
-export default function ThemeProvider({ children }) {
-  const defaultTheme = localStorage.getItem(THEME_KEY) || 'dark'
-
-  const [theme, setTheme] = useState(defaultTheme)
-
-  const currentTheme = useMemo(() => themes[theme] || themes.dark, [theme])
-
-  function handleToggleTheme() {
-    const nextTheme = theme === 'dark' ? 'light' : 'dark'
-
-    setTheme(nextTheme)
-
-    localStorage.setItem(THEME_KEY, nextTheme)
+export default class ThemeProvider extends Component {
+  state = {
+    theme: localStorage.getItem(THEME_KEY) || 'dark',
   }
 
-  return (
-    <ThemeContext
-      value={{
-        theme,
-        onToggleTheme: handleToggleTheme,
-      }}
-    >
-      <StyledComponentsThemeProvider theme={currentTheme}>
-        {children}
-      </StyledComponentsThemeProvider>
-    </ThemeContext>
-  )
+  handleToggleTheme = () => {
+    const nextTheme = this.state.theme === 'dark' ? 'light' : 'dark'
+
+    this.setState({ theme: nextTheme }, () => {
+      localStorage.setItem(THEME_KEY, nextTheme)
+    })
+  }
+
+  render() {
+    return (
+      <ThemeContext
+        value={{
+          theme: this.state.theme,
+          onToggleTheme: this.handleToggleTheme,
+        }}
+      >
+        <StyledComponentsThemeProvider theme={themes[this.state.theme]}>
+          {this.props.children}
+        </StyledComponentsThemeProvider>
+      </ThemeContext>
+    )
+  }
 }
 
 export function useTheme() {
