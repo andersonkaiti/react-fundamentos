ae677d09ed638fbbe4e01cd3a7ecba465820fc0f
andersonkaiti <anderkaiti@gmail.com>
2026-01-03 00:52:54 -0300

    :sparkles: feat(Header,hoc): implement higher-order component for theme context and add componentDidUpdate for theme change logging

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,38 @@
-import { Component } from 'react'
+import React, { Component } from 'react'
 import { ThemeContext } from '../../contexts/ThemeProvider'
 import { Container } from './styles'
 
-export default class Header extends Component {
+function HOC(WrappedComponent) {
+  return class HocComponent extends React.Component {
+    render() {
+      return (
+        <ThemeContext.Consumer>
+          {(ctx) => <WrappedComponent {...this.props} {...ctx} />}
+        </ThemeContext.Consumer>
+      )
+    }
+  }
+}
+
+class Header extends Component {
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.theme !== prevProps.theme) {
+      console.log('theme changed')
+    }
+  }
+
   render() {
+    const { theme, onToggleTheme } = this.props
+
     return (
-      <ThemeContext.Consumer>
-        {({ theme, onToggleTheme }) => (
-          <Container>
-            <h1>JStack's Blog</h1>
-            <button onClick={onToggleTheme} type="button">
-              {theme === 'dark' ? '☀️' : '🌙'}
-            </button>
-          </Container>
-        )}
-      </ThemeContext.Consumer>
+      <Container>
+        <h1>JStack's Blog</h1>
+        <button onClick={onToggleTheme} type="button">
+          {theme === 'dark' ? '☀️' : '🌙'}
+        </button>
+      </Container>
     )
   }
 }
+
+export default HOC(Header)
