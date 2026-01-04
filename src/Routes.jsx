68447397545e62af2865c26dea0dc02cd8e5c216
andersonkaiti <anderkaiti@gmail.com>
2026-01-03 22:47:14 -0300

    :sparkles: feat(routing,NotFound): implement NotFound page and utilize Switch in Routes for exclusive rendering

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,12 +1,16 @@
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import Home from './pages/Home'
+import NotFound from './pages/NotFound'
 import Posts from './pages/Posts'
+
+// Para que o React Router renderize apenas 1 página por vez, usamos o Switch
 
 export default function Routes() {
   return (
-    <>
+    <Switch>
       <Route component={Home} exact path="/" />
       <Route component={Posts} path="/posts" />
-    </>
+      <Route component={NotFound} />
+    </Switch>
   )
 }
